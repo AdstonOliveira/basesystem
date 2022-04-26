7f4f673fdb554b5505ba4dbf06645822b3455575
Adston <aepdesenvolvimento@gmail.com>
2022-04-26 13:13:07 -0300

    pequenos ajustes

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginApiRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
 
@@ -14,19 +15,33 @@ class LoginController extends Controller
 
 
     public function login(LoginApiRequest $request){
-
         $user = User::where("email", $request->input("email"))->first();
 
-        if (! $user || ! Hash::check($request->input("password"), $user->password)) {
+
+
+        if (! $user ) {
             throw ValidationException::withMessages([
                 'email' => ['The provided credentials are incorrect.'],
             ]);
+        }else{
+
+            $credentials= [
+                "email"     => $request->input("email"),
+                "password"  => $request->input("password")
+            ];
+
+            if( !Auth::attempt($credentials) ){
+                throw ValidationException::withMessages([
+                    'email' => ['Dados incorretos.'],
+                ]);
+            }else{
+                auth()->user()->tokens()->delete();
+                return auth()->user()->createToken($request->device_name)->plainTextToken;
+            }
         }
 
-        if( $user->currentAccessToken() )
-            $user->currentAccessToken()->delete();
 
-        return $user->createToken($request->device_name)->plainTextToken;
+
     }
 
 
